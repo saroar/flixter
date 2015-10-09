d10fa6e524733a77761f39e63e263d09e65403fd
Saroar <saroar9@gmail.com>
2015-10-09 17:35:40 +0000

    add paginate for font page

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'mini_magick', '~> 4.3.3'
 gem 'stripe', '~> 1.27.2'
 gem 'ranked-model', '~> 0.4.0'
 gem 'jquery-ui-rails', '~> 5.0.5'
+gem 'will_paginate', '~> 3.0.7'
 
 
 group :doc do
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def index
-		@courses = Course.all
+		@courses = Course.all.order("created_at").paginate(:page => params[:page], :per_page => 6 )
 	end
 
   def privacy
