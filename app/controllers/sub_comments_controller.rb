b568e843f3beb61ac9441c827a9bd50045bd46a3
Saroar <saroar9@gmail.com>
2015-10-27 21:40:01 +0000

    fixed sub_controller

--- app/controllers/sub_comments_controller.rb
+++ app/controllers/sub_comments_controller.rb
@@ -2,14 +2,12 @@ class SubCommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @lesson = Lesson.find(params[:lesson_id])
     @comment = Comment.find(params[:comment_id])
     @comment.sub_comments.create(sub_comment_params.merge(:user => current_user))
     redirect_to comment_path(@comment)
   end
 
   def show
-    @lesson = Lesson.find(params[:lesson_id])
     @comment = Comment.find(params[:comment_id])
     @sub_comment = SubComment.find(params[:id])
   end
